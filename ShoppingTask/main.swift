fc9a4f8b7b767c38576aed5ef18f22bd19fcc5cd
andymuncey <andrewmuncey@Andrews-MacBook-Pro.local>
2015-06-09 12:27:35 +0100

    Initial Commit

---
+++ ShoppingTask/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ShoppingTask
+//
+//  Created by Andrew Muncey on 09/06/2015.
+//  Copyright (c) 2015 University of Chester. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
